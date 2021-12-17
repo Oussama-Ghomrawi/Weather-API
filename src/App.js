60570eac904b4f59709fc27bcb0f6a365683f941
Yahya <ydarwich18@gmail.com>
2021-12-17 13:08:20 +0200

    Weather API: HTML/CSS Responsive Design

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import React from "react";
 
 // import SayHi, { SayHello } from "./components/WeatherItem";
 // import fakeWeatherData from "./fakeWeatherData.json";
-// import cloudy from "./img/weather-icons/cloudy.svg";
-// import clear from "./img/weather-icons/clear.svg";
-// import drizzle from "./img/weather-icons/drizzle.svg";
-// import fog from "./img/weather-icons/fog.svg";
-// import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
-// import partlycloudy from "./img/weather-icons/partlycloudy.svg";
-// import rain from "./img/weather-icons/rain.svg";
-// import snow from "./img/weather-icons/snow.svg";
-// import storm from "./img/weather-icons/storm.svg";
-// import unknown from "./img/weather-icons/unknown.svg";
 
-import placeholder from "./img/weather-icons/shawerma.jpg";
+import cloudy from "./img/weather-icons/cloudy.svg";
+import clear from "./img/weather-icons/clear.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
+import snow from "./img/weather-icons/snow.svg";
+import storm from "./img/weather-icons/storm.svg";
+import unknown from "./img/weather-icons/unknown.svg";
+
+// import placeholder from "./img/weather-icons/shawerma.jpg";
 import "./App.css";
 
 function App() {
@@ -35,19 +36,19 @@ function App() {
       </header>
       <body className="weather-content">
         <div className="real-weather">
-          <img className="live-weather" src={placeholder} />
+          <img className="live-weather" src={mostlycloudy} alt="mostlycloudy icon" />
           <p className="weather-status"> overcast clouds</p>
           <span className="temperature">
-            {" "}
+            
             <b>Temperature</b>10&deg; to 11&deg;c
           </span>
           <div>
             <span className="humidity">
-              {" "}
+              
               <b>Humidity </b>78%
             </span>
             <span className="pressure">
-              {" "}
+            
               <b>Pressure </b>1008.48
             </span>
           </div>
@@ -55,37 +56,37 @@ function App() {
         <div className="full-day-weather">
           <div className="weather-3">
             <span>03:00</span>
-            <img className="image-3" src={placeholder} />
+            <img className="image-3" src={mostlycloudy} alt="mostlycloudy icon"/>
             <span>8°C</span>
           </div>
           <div className="weather-6">
             <span>06:00</span>
-            <img className="image-6" src={placeholder} />
+            <img className="image-6" src={mostlycloudy} alt="mostlycloudy icon"/>
             <span>9°C</span>
           </div>
           <div className="weather-9">
             <span>09:00</span>
-            <img className="image-9" src={placeholder} />
+            <img className="image-9" src={clear} alt="clear icon"/>
             <span>14°C</span>
           </div>
           <div className="weather-12">
             <span>12:00</span>
-            <img className="image-12" src={placeholder} />
+            <img className="image-12" src={clear} alt="clear icon"/>
             <span>17°C</span>
           </div>
           <div className="weather-15">
             <span>15:00</span>
-            <img className="image-15" src={placeholder} />
+            <img className="image-15" src={clear} alt="clear icon"/>
             <span>18°C</span>
           </div>
           <div className="weather-18">
             <span>18:00</span>
-            <img className="image-18" src={placeholder} />
+            <img className="image-18" src={clear} alt="clear icon"/>
             <span>16°C</span>
           </div>
           <div className="weather-21">
             <span>21:00</span>
-            <img className="image-21" src={placeholder} />
+            <img className="image-21" src={mostlycloudy} alt="mostlycloudy icon"/>
             <span>13°C</span>
           </div>
         </div>
